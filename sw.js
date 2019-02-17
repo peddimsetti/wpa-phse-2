8f11e7905b788f1745234c4176c5dba2ff89bff5
peddimsetti <vamsijanakiram007@gmail.com>
2019-02-17 11:35:13 +0530

    hello

---
+++ sw.js
@@ -0,0 +1,26 @@
+var cache1="c1";
+this.addEventListener("install",function(event) {
+	event.waitUntil(
+
+		caches.open(cache1).then(cache=>{
+			cache.addAll([
+				"1.css",
+				"form.html",
+				"resume.css",
+				"resume.html"
+				])
+		})
+		);
+});
+
+this.addEventListener('fetch',function(event){
+	event.respondWith(
+caches.open(cache1).then(function(cache){
+	return cache.match(event.request).then(function(res){
+		return res || fetch(event.request).then(function(reqres){
+			cache.put(event.request,reqres.clone());
+		})
+	})
+})
+		)
+})
